e92d446cf8c646c47121f6bffbafe7894f6256c8
João Pedro Toledo Leite <105946478+jptoledo02@users.noreply.github.com>
2023-07-14 20:32:46 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () =>{
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+
+        imagemPersonagemGrande.src = `./src/imgs/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descPersonagem = document.getElementById('descricao-personagem');
+
+        descPersonagem.innerText = personagem.getAttribute('data-description');
+
+    })
+})
